10eef59e4a5f7f4e3d50d186c59f78a97d871626
Jaskiran Singh <jsingh8986@yahoo.com>
2022-12-21 19:01:32 -0800

    commit tree 9f194ef90347655513b9ef40b0ac6ed720607534

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your code here!
+// Array
+const books = ['book1', 'book2', 'book3']
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+//DOM Selectors
+const div = document.createElement('div');
+const ul = document.createElement('ul');
+const selectDiv = document.querySelector('div')
+const li = document.querySelectorAll('li');
+const newHeader = document.getElementById("main");
+
+//DOM Appenders
+document.body.append(div);
+
+
+//FOR Statments
+for (let i=0; i < 3; i++) {
+    const li = document.createElement('li');
+    li.textContent = (books[i]);
+    ul.append(li)
+    div.append(ul);
+}
+
+for (let i = 0; i < colors.length; i++) {
+    const li = document.createElement('li');
+    li.textContent = (colors[i]);
+    ul.append(li)
+    
+}
+
+//Styles
+
+ul.style.backgroundColor = 'green'
+ul.style.lineHeight = 2;
+newHeader.textContent = 'Jaz is the champion!'
+newHeader.className = 'colors'
+
+//removing Selectors
+const ulSelector = document.querySelector('ul');
+const secondChild = ulSelector.querySelector("li:nth-child(2)");
+ulSelector.removeChild(secondChild)
+
+ul.remove()
